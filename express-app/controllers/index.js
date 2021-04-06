98f53379cd7bbfb101a7de2eeb5b938422e17004
JaganKaartik <kaartikjagan@gmail.com>
2021-04-06 18:07:55 +0530

    Updated Controllers/Routes -- Total Status

--- express-app/controllers/index.js
+++ express-app/controllers/index.js
@@ -1,5 +1,6 @@
 const Car = require('../models/car');
 const Parking = require('../models/parking');
+const slot = require('../models/slot');
 let Slots = require('../models/slot');
 
 const createParkingSlot = (req, res) => {
@@ -9,16 +10,38 @@ const createParkingSlot = (req, res) => {
 };
 
 const parkCar = async (req, res) => {
-  const carnumber = req.query.carnumber;
-  const color = req.query.color;
-  await Car.create({ carRegNo: carnumber, carColor: color });
-  const avbl_slot = Slots.indexOf(1) + 1;
-  Slots[avbl_slot - 1] = 0;
-  await Parking.create({
-    carRegNo: carnumber,
-    slotNo: avbl_slot,
-  });
-  res.send(`Allocated Slot no ${avbl_slot}`);
+  const slotExists = Slots.indexOf(1);
+  if (slotExists == -1) {
+    res.send(`Sorry, parking lot is full `);
+  } else {
+    const carnumber = req.query.carnumber;
+    const color = req.query.color;
+
+    const carExists = await Car.find({ carRegNo: carnumber }).then((resp) => {
+      return resp.length >= 1 ? true : false;
+    });
+
+    if (!carExists) {
+      await Car.create({
+        carRegNo: carnumber,
+        carColor: color,
+      });
+      const avbl_slot = Slots.indexOf(1) + 1;
+      Slots[avbl_slot - 1] = 0;
+      await Parking.create({
+        carRegNo: carnumber,
+        slotNo: avbl_slot,
+      });
+      res.send(`Allocated Slot no ${avbl_slot}`);
+    } else {
+      res.send(`Error! Car already parked`);
+    }
+  }
 };
 
-module.exports = { createParkingSlot, parkCar };
+const status = async (req, res) => {
+  const parkingStatus = await Parking.find({});
+  res.send(parkingStatus);
+};
+
+module.exports = { createParkingSlot, parkCar, status };
--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createParkingSlot, parkCar } = require('../controllers');
+const { createParkingSlot, parkCar, status } = require('../controllers');
 
 router.get('/create_parking_lot', createParkingSlot);
 router.get('/park', parkCar);
+router.get('/status', status);
+router.get(
+  '/registration_numbers_for_cars_with_colour/:color',
+  parkingStatusRegistrationColor
+);
+router.get('/status', status);
 
 module.exports = router;
