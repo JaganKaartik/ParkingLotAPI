575741a14b57453e7de3f9565d13aa0b056e4605
JaganKaartik <kaartikjagan@gmail.com>
2021-04-07 22:46:36 +0530

    Updated Test Scripts

--- express-app/test/api.test.js
+++ express-app/test/api.test.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const Car = require('../models/car');
 const Parking = require('../models/parking');
+const { connectDB } = require('../config/database.config');
 
 // Assertions
 let should = chai.should();
@@ -19,6 +20,24 @@ const carNumberArray = [
 ];
 
 const carNoArray = ['MH01HH8888', 'MH01HH2771'];
+
+before(function () {
+  connectDB();
+});
+
+// after(() => {
+//   Car.deleteMany({}, function (err) {
+//     console.log('collection removed');
+//   });
+//   Parking.deleteMany({}, function (err) {
+//     console.log('collection removed');
+//   });
+// });
+
+/*
+ API Test
+ /create_parking_lot
+*/
 
 describe('Test GET route /create_parking_lot', () => {
   it('It should create a parking lot with No of Slots (parameter) > 0', (done) => {
@@ -48,23 +67,26 @@ describe('Test GET route /create_parking_lot', () => {
   });
 });
 
+/*
+ API Test
+ /park
+*/
+
 describe('Test GET route /park ', () => {
   // Create Parking Lot with 6 Slots
   it('Creating Parking Lot with 6 slots', (done) => {
-    chai.request(server).get('/create_parking_lot?number=3');
+    chai.request(server).get('/create_parking_lot?number=6');
     done();
   });
   // Park Car in Avbl slots
   it('It should park car in the nearest avbl slot', (done) => {
     for (i = 0; i < 6; ++i) {
       let value = i;
-      console.log(`/park?carnumber=${carNumberArray[i]}&color=White`);
       chai
         .request(server)
-        .get(`/park?carnumber=${carNumberArray[i]}&color=White`)
+        .get(`/park?carnumber=${carNumberArray[value]}&color=White`)
         .end((err, response) => {
           const regex = new RegExp('Allocated Slot number: [0-9]*');
-          console.log(`${carNumberArray[value]} and ${response.text}`);
           const result = response.text.match(regex);
           should.exist(result);
           response.should.have.status(200);
@@ -72,45 +94,39 @@ describe('Test GET route /park ', () => {
     }
     done();
   });
-
-  it('Clear DB', (done) => {
-    Car.deleteMany({}, function (err) {
-      console.log('collection removed');
-    });
-    Parking.deleteMany({}, function (err) {
-      console.log('collection removed');
-    });
-
-    done();
-  });
 });
 
-// it('It should not create a parking lot with No of Slots (parameter) = 0', (done) => {
-//   chai
-//     .request(server)
-//     .get('/create_parking_lot?number=0')
-//     .end((err, response) => {
-//       const regex = new RegExp('Created a parking lot with [0-9]* slots');
-//       const result = response.text.match(regex);
-//       expect(result).to.be.null;
-//       response.should.have.status(400);
-//       done();
-//     });
-// });
+/*
+ API Test
+ /leave
+*/
 
-// carNumberArray.forEach((value) => {
-//   it('It should park car in the nearest avbl slot', (done) => {
-//     console.log(`/park?carnumber=${value}&color=White`);
-//     chai
-//       .request(server)
-//       .get(`/park?carnumber=${value}&color=White`)
-//       .end((err, response) => {
-//         const regex = new RegExp('Allocated Slot number: [0-9]*');
-//         console.log(`${value} and ${response.text}`);
-//         const result = response.text.match(regex);
-//         should.exist(result);
-//         response.should.have.status(200);
-//       });
-//     done();
-//   });
-// });
+describe('Test GET route /leave', () => {
+  //Leave Slots
+  it('It should leave Slot Number Empty', (done) => {
+    chai
+      .request(server)
+      .get('/leave?slot=6')
+      .end((err, response) => {
+        // const regex = new RegExp('Slot number [0-9]* is free');
+        // const result = response.text.match(regex);
+        // should.exist(result);
+        console.log(response.text);
+        response.should.have.status(200);
+        done();
+      });
+  });
+
+  it('It should not leave slot when incorrect slot no', (done) => {
+    chai
+      .request(server)
+      .get('/leave?slot=10')
+      .end((err, response) => {
+        const expectedResponse = 'Slot Incorrect';
+        const result = response.text === expectedResponse ? true : false;
+        expect(result).to.be.true;
+        response.should.have.status(404);
+        done();
+      });
+  });
+});
