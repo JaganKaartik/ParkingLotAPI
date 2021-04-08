c5879544a9e994750483642f51087f6fde292409
JaganKaartik <kaartikjagan@gmail.com>
2021-04-08 15:16:16 +0530

    PORT updated

--- express-app/config/default.config.js
+++ express-app/config/default.config.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
-const { PORT, MONGO_URL, MONGO_URL_TEST } = process.env;
+const { PORT_TEST, PORT_MAIN, MONGO_URL, MONGO_URL_TEST } = process.env;
 
 const MONGO_DB_URL =
   process.env.NODE_ENV === 'test' ? MONGO_URL_TEST : MONGO_URL;
+
+const PORT = process.env.NODE_ENV === 'test' ? PORT_TEST : PORT_MAIN;
 
 module.exports = { PORT, MONGO_DB_URL };
